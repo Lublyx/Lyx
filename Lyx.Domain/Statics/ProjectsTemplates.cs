b0251cedddb0234c3dc54549a680788e07282b3e
Lublyx <lucas.sertelon@gmail.com>
2026-06-17 00:01:25 +0200

    Added native html template

--- Lyx.Domain/Statics/ProjectsTemplates.cs
+++ Lyx.Domain/Statics/ProjectsTemplates.cs
@@ -47,4 +47,26 @@ if __name__ == '__main__':
     App().run()
 ";
     }
+
+    public static class Html
+    {
+        public const string Native = @"
+<!DOCTYPE html>
+<html lang='fr'>
+  <head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+    <title>My Website</title>
+    <link rel='stylesheet' href='./style.css'>
+    <link rel='icon' href='./favicon.ico' type='image/x-icon'>
+  </head>
+  <body>
+    <main>
+        <h1>Welcome to My Website</h1>  
+    </main>
+  </body>
+</html>
+";
+    }
 }
